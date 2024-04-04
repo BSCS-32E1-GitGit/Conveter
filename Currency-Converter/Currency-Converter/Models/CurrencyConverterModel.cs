98e51c5bf3eff595bf73aded7a53e922e7cdddac
Jonas R <75775925+Rizardo-Jonas-BSCS32E1@users.noreply.github.com>
2024-04-04 19:26:18 +0800

    Currency Conveter Done

---
+++ Currency-Converter/Currency-Converter/Models/CurrencyConverterModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class CurrencyConverterModel
+{
+    [Required(ErrorMessage = "Please enter the amount")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive number")]
+    public double Amount { get; set; }
+
+    [Required(ErrorMessage = "Please select the currency from")]
+    public string FromCurrency { get; set; }
+
+    [Required(ErrorMessage = "Please select the currency to")]
+    public string ToCurrency { get; set; }
+}
